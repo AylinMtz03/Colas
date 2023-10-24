13de257470d9d0c519775bf58beded1fd211837e
unknown <martinezsantosaylin6@gmail.com>
2023-10-24 09:23:07 -0600

    Commit Aylin

--- Colas.R
+++
@@ -1,8 +0,0 @@
-# Correr los siguientes comandos, comentar la linea y subir un archivo con su nombre
-cola <- list() #Para crear una lista usamos la función list() , que nos pedirá los elementos que deseamos incluir en nuestra lista.
-cola <- c(cola, "Dato 1") #La c es de «combinar» (combinar uno o más valores o elementos)
-print(cola) #Se utiliza para imprimir lo que tiene  en la lista
-
-Dato_eliminado <- cola[1] #Se utiliza para eliminar un elemento de la lista
-cola <- cola[-1] #devuelve todos excepto el primer elemento
-print(cola) #Se utiliza para imprimir lo que tiene en la lista
